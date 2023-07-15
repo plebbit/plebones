dfe970cc8aa7b0265c2c854f105814d4d75a0c95
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-07-15 15:22:51 +0000

    put themes css variables in themes.css

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
+import './themes.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
