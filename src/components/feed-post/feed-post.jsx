4b97fb827bb569dd10755599c2aeaa4132aea9fe
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-08-09 19:17:59 +0000

    display pending edits in feed view

--- src/components/feed-post/feed-post.jsx
+++ src/components/feed-post/feed-post.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import styles from './feed-post.module.css'
 import Arrow from '../icons/arrow'
 import PostTools from '../post-tools'
-import {useBlock, useAuthorAddress} from '@plebbit/plebbit-react-hooks'
+import {useBlock, useAuthorAddress, useEditedComment} from '@plebbit/plebbit-react-hooks'
 import useUnreadReplyCount from '../../hooks/use-unread-reply-count'
 import useUpvote from '../../hooks/use-upvote'
 import useDownvote from '../../hooks/use-downvote'
+import useCommentLabels from '../../hooks/use-comment-labels'
 
 const FeedPostMedia = ({mediaInfo}) => {
   if (!mediaInfo) {
@@ -25,6 +26,12 @@ const FeedPostMedia = ({mediaInfo}) => {
 }
 
 const FeedPost = ({post, index}) => {
+  // handle pending mod or author edit
+  const {state: editedPostState, editedComment: editedPost} = useEditedComment({comment: post})
+  if (editedPost) {
+    post = editedPost
+  }
+
   let hostname
   try {
     hostname = new URL(post?.link).hostname.replace(/^www\./, '')
@@ -50,6 +57,8 @@ const FeedPost = ({post, index}) => {
   const largeScoreNumber = String(scoreNumber).length > 3
   const negativeScoreNumber = scoreNumber < 0
 
+  const labels = useCommentLabels(post, editedPostState)
+
   return <div className={styles.feedPost}>
     <div className={styles.textWrapper}>
       <div className={styles.column}>
@@ -65,7 +74,8 @@ const FeedPost = ({post, index}) => {
       </div>
       <div className={[styles.column, hidden ? styles.hidden : undefined].join(' ')}>
         <div className={styles.header}>
-          <Link to={internalLink} className={styles.title}>{post?.title || post?.content || '-'}</Link>
+          <Link to={internalLink} className={styles.title}>{(post?.title || post?.content || '-').trim()}</Link>
+          {labels.map(label => <>{' '}<span key={label} className={styles.label}>{label}</span></>)}
           {hostname && <Link to={post?.link} target='_blank' rel='noreferrer'> {hostname}</Link>}
         </div>
         <div className={styles.content}>
