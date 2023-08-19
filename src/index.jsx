c7b4ca9b4a572ba6fdb6fcc0af5f8a1c2f6a1514
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-08-19 20:20:47 +0000

    setup native apps

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,13 @@ import App from './app'
 import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 import './themes.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import PlebbitJs from '@plebbit/plebbit-js'
+// inject native functions into renderer
+// https://github.com/plebbit/plebbit-js/blob/master/docs/cross-platform-native-functions.md
+if (window.plebbitJsNativeFunctions) {
+  PlebbitJs.setNativeFunctions(window.plebbitJsNativeFunctions)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -13,3 +20,6 @@ root.render(
     </Router>
   </React.StrictMode>
 )
+
+// set up PWA https://cra.link/PWA
+serviceWorkerRegistration.register()
