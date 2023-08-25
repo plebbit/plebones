2b967997e124984f72401d5521633b5857f60fe5
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-08-25 20:37:55 +0000

    fix electron web security bug

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,8 @@
 // force IpfsHttpClient to use node-fetch to fix max 6 connections
 // temporary, eventually we will fork ipfs-http-client and replace
 // 'native-fetch'
-const mock = require('mock-require');
-const fetch = require('node-fetch');
+const mock = require('mock-require')
+const fetch = require('node-fetch')
 mock('ipfs-utils/src/env.js', {
   isTest: false,
   isElectron: false,
@@ -13,20 +13,20 @@ mock('ipfs-utils/src/env.js', {
   isWebWorker: false,
   isEnvWithDom: false,
   isReactNative: false,
-});
+})
 mock('native-fetch', {
   default: fetch.default,
   Headers: fetch.Headers,
   Request: fetch.Request,
   Response: fetch.Response,
-});
+})
 
-const envPaths = require('env-paths').default('plebones', { suffix: false });
+const envPaths = require('env-paths').default('plebones', { suffix: false })
 const { contextBridge } = require('electron')
 const path = require('path')
 
 // dev uses http://localhost, prod uses file://...index.html
-const isDev = window.location.protocol === 'http:';
+const isDev = window.location.protocol === 'http:'
 
 const defaultPlebbitOptions = {
   // find the user's OS data path
@@ -36,18 +36,14 @@ const defaultPlebbitOptions = {
   pubsubHttpClientsOptions: ['http://localhost:5001/api/v0'] || undefined,
   // electron starts the local ipfs gateway on port 11028 because 8080 is too common
   ipfsGatewayUrls: ['http://localhost:11028'] || undefined,
-};
+}
 
 // expose a flag to indicate that we are running in electron
-contextBridge.exposeInMainWorld('electron', { isElectron: true });
+contextBridge.exposeInMainWorld('electron', { isElectron: true })
 
 // expose plebbit-js native functions into electron's renderer
 contextBridge.exposeInMainWorld('plebbitJsNativeFunctions', require('@plebbit/plebbit-js').nativeFunctions.node)
 contextBridge.exposeInMainWorld('defaultPlebbitOptions', defaultPlebbitOptions)
 
-// try/catch localStorage.debug because causes unknown error sometimes
-// try {
-//   if (isDev) localStorage.debug = 'plebbit-js:*,plebbit-react-hooks:*,plebones:*';
-// } catch (e) {
-//   console.log(e);
-// }
+// uncomment for logs
+// localStorage.debug = 'plebbit-js:*,plebbit-react-hooks:*,plebones:*'
