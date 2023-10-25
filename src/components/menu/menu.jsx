9e9dac65943c46d790520a1e3bb84cdfed482345
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-10-25 18:50:11 +0000

    refactor(menu): max the time filter select value to 4 chars so it's not too long

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -107,7 +107,7 @@ const Menu = () => {
           </select>{' '}
           <select onChange={changeTimeFilter} className={[styles.feedName, styles.menuItem].join(' ')} value={selectedTimeFilterName}>
             {timeFilterNames.map((timeFilterName) => (
-              <option value={timeFilterName}>{timeFilterName}</option>
+              <option value={timeFilterName}>{timeFilterName.substring(0, 4)}</option>
             ))}
           </select>{' '}
           <Link to={showCatalogLink ? catalogLink : feedLink} className={styles.menuItem}>
