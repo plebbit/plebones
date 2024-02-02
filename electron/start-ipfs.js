14f0bac87d75e5081211a0df3561e212d95aa71e
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-02-02 15:13:55 +0000

    fix(electron): make sure ipfs repo is migrated when starting ipfs

--- electron/start-ipfs.js
+++ electron/start-ipfs.js
@@ -54,6 +54,11 @@ const startIpfs = async () => {
     await spawnAsync(ipfsPath, ['init'], {env, hideWindows: true})
   } catch (e) {}
 
+  // make sure repo is migrated
+  try {
+    await spawnAsync(ipfsPath, ['repo', 'migrate'], {env, hideWindows: true})
+  } catch (e) {}
+
   // dont use 8080 port because it's too common
   await spawnAsync(ipfsPath, ['config', '--json', 'Addresses.Gateway', '"/ip4/127.0.0.1/tcp/6473"'], {
     env,
