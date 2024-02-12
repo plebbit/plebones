6daa63ee06937bf67e975e2bdafe9a4f73e20c39
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-02-12 02:20:26 +0000

    feat(settings): implement linking crypto wallets

---
+++ src/views/settings/crypto-wallets/crypto-wallets.jsx
@@ -0,0 +1,130 @@
+import {useState} from 'react'
+import {useAccount, setAccount} from '@plebbit/plebbit-react-hooks'
+
+const getWalletMessageToSign = (authorAddress, timestamp, address) => {
+  let messageToSign = {}
+  // the property names must be in this order for the signature to match
+  // insert props one at a time otherwise babel/webpack will reorder
+  messageToSign.domainSeparator = 'plebbit-author-wallet'
+  messageToSign.authorAddress = authorAddress
+  messageToSign.timestamp = timestamp
+  messageToSign.address = address
+  // use plain JSON so the user can read what he's signing
+  messageToSign = JSON.stringify(messageToSign)
+  return messageToSign
+}
+
+const CryptoWalletsForm = ({account}) => {
+  // force account to be defined to be able to use account as default values
+  if (!account) {
+    throw Error('CryptoWalletsForm account prop must be defined')
+  }
+  const authorAddress = account?.author?.address
+  const defaultWalletsArray = Object.keys(account?.author?.wallets || {}).map((chainTicker) => ({
+    chainTicker,
+    address: account?.author?.wallets?.[chainTicker]?.address,
+    timestamp: account?.author?.wallets?.[chainTicker]?.timestamp,
+    signature: account?.author?.wallets?.[chainTicker]?.signature?.signature,
+  }))
+  if (!defaultWalletsArray.length) {
+    defaultWalletsArray.push({})
+  }
+  const [walletsArray, setWalletsArray] = useState(defaultWalletsArray)
+  const setWalletsArrayProperty = (index, property, value) => {
+    const newArray = [...walletsArray]
+    newArray[index] = {...newArray[index], [property]: value}
+    setWalletsArray(newArray)
+  }
+
+  const walletsInputs = walletsArray.map((wallet, index) => (
+    <div>
+      <input onChange={(e) => setWalletsArrayProperty(index, 'chainTicker', e.target.value)} value={wallet.chainTicker} placeholder="chain ticker e.g. eth, matic..." />
+      <input onChange={(e) => setWalletsArrayProperty(index, 'address', e.target.value)} value={wallet.address} placeholder="token address e.g. 0x..." />
+      <button
+        onClick={() => {
+          if (!wallet.chainTicker) {
+            return alert('missing chain ticker')
+          }
+          if (!wallet.address) {
+            return alert('missing address')
+          }
+          const timestamp = wallet.timestamp || Math.floor(Date.now() / 1000)
+          const messageToSign = getWalletMessageToSign(authorAddress, timestamp, wallet.address)
+
+          // if timestamp changed, update it so it gets saved properly
+          if (timestamp !== wallet.timestamp) {
+            setWalletsArrayProperty(index, 'timestamp', timestamp)
+          }
+
+          navigator.clipboard.writeText(messageToSign)
+          alert(messageToSign)
+        }}
+      >
+        copy message to sign
+      </button>
+      <input onChange={(e) => setWalletsArrayProperty(index, 'signature', e.target.value)} value={wallet.signature} placeholder="signature e.g. 0x..." />
+      <div>
+        go to{' '}
+        <a href="https://etherscan.io/verifiedSignatures" target="_blank" rel="noreferrer">
+          https://etherscan.io/verifiedSignatures
+        </a>{' '}
+        and sign, then paste signature
+      </div>
+    </div>
+  ))
+
+  const save = () => {
+    const wallets = {}
+    for (const wallet of walletsArray) {
+      if (!wallet.chainTicker) {
+        continue
+      }
+      if (!wallet.address) {
+        return alert(`${wallet.chainTicker} missing address`)
+      }
+      if (!wallet.signature) {
+        return alert(`${wallet.chainTicker} missing signature`)
+      }
+      if (!wallet.timestamp) {
+        return alert(`${wallet.chainTicker} missing timestamp, must click "copy message to sign" again and resign`)
+      }
+      wallets[wallet.chainTicker] = {
+        address: wallet.address,
+        timestamp: wallet.timestamp,
+        signature: {
+          signature: wallet.signature,
+          type: 'eip191',
+        },
+      }
+    }
+    setAccount({...account, author: {...account?.author, wallets}})
+    alert(`saved`)
+  }
+
+  return (
+    <>
+      {walletsInputs}
+      <button onClick={() => setWalletsArray([...walletsArray, {}])}>+add</button>
+      <button onClick={save}>save</button>
+    </>
+  )
+}
+
+const CryptoWallets = () => {
+  const account = useAccount()
+  if (!account) {
+    return (
+      <div>
+        <div>wallets:</div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div>wallets:</div>
+      <CryptoWalletsForm account={account} />
+    </div>
+  )
+}
+
+export default CryptoWallets
---
+++ src/views/settings/nft-avatar/nft-avatar.jsx
@@ -0,0 +1,145 @@
+import {useMemo, useState} from 'react'
+import styles from './nft-avatar.module.css'
+import {useAccount, setAccount, useAuthorAvatar} from '@plebbit/plebbit-react-hooks'
+
+const NftAvatarPreview = ({avatar}) => {
+  const account = useAccount()
+  let author = useMemo(() => ({...account?.author, avatar}), [account, avatar])
+
+  // if avatar already set, and user hasn't typed anything yet, preview already set author
+  if (account?.author?.avatar && !avatar?.chainTicker && !avatar?.address && !avatar?.id && !avatar?.signature) {
+    author = account.author
+  }
+
+  const {imageUrl, state, error} = useAuthorAvatar({author})
+
+  // not enough data to preview yet
+  if (!author?.avatar?.address && !author?.avatar?.signature) {
+    return
+  }
+
+  const stateText = state !== 'succeeded' ? `${state}...` : undefined
+
+  return (
+    <div>
+      <div>avatar preview:</div>
+      <img className={styles.nftAvatarPreviewImage} alt="" src={imageUrl} /> {stateText} {error?.message}
+    </div>
+  )
+}
+
+const getNftMessageToSign = (authorAddress, timestamp, tokenAddress, tokenId) => {
+  let messageToSign = {}
+  // the property names must be in this order for the signature to match
+  // insert props one at a time otherwise babel/webpack will reorder
+  messageToSign.domainSeparator = 'plebbit-author-avatar'
+  messageToSign.authorAddress = authorAddress
+  messageToSign.timestamp = timestamp
+  messageToSign.tokenAddress = tokenAddress
+  messageToSign.tokenId = String(tokenId) // must be a type string, not number
+  // use plain JSON so the user can read what he's signing
+  messageToSign = JSON.stringify(messageToSign)
+  return messageToSign
+}
+
+const NftAvatarForm = ({account}) => {
+  // force account to be defined to be able to use account as default values
+  if (!account) {
+    throw Error('NftAvatarForm account prop must be defined')
+  }
+  const authorAddress = account?.author?.address
+  const [chainTicker, setChainTicker] = useState(account?.author?.avatar?.chainTicker)
+  const [tokenAddress, setTokenAddress] = useState(account?.author?.avatar?.address)
+  const [tokenId, setTokenId] = useState(account?.author?.avatar?.id)
+  const [timestamp, setTimestamp] = useState(account?.author?.avatar?.timestamp)
+  const [signature, setSignature] = useState(account?.author?.avatar?.signature?.signature)
+
+  const copyMessageToSign = () => {
+    if (!chainTicker) {
+      return alert('missing chain ticker')
+    }
+    if (!tokenAddress) {
+      return alert('missing token address')
+    }
+    if (!tokenId) {
+      return alert('missing token id')
+    }
+    const newTimestamp = Math.floor(Date.now() / 1000)
+    const messageToSign = getNftMessageToSign(authorAddress, newTimestamp, tokenAddress, tokenId)
+    // update timestamp every time the user gets a new message to sign
+    setTimestamp(newTimestamp)
+    navigator.clipboard.writeText(messageToSign)
+    alert(messageToSign)
+  }
+
+  // how to resolve and verify NFT signatures https://github.com/plebbit/plebbit-js/blob/master/docs/nft.md
+  const avatar = {
+    chainTicker: chainTicker?.toLowerCase() || account?.author?.avatar?.chainTicker,
+    timestamp,
+    address: tokenAddress || account?.author?.avatar?.address,
+    id: tokenId || account?.author?.avatar?.id,
+    signature: {
+      signature: signature || account?.author?.avatar?.signature?.signature,
+      type: 'eip191',
+    },
+  }
+
+  const save = () => {
+    if (!chainTicker) {
+      return alert('missing chain ticker')
+    }
+    if (!tokenAddress) {
+      return alert('missing token address')
+    }
+    if (!tokenId) {
+      return alert('missing token id')
+    }
+    if (!signature) {
+      return alert('missing signature')
+    }
+    setAccount({...account, author: {...account?.author, avatar}})
+    alert(`saved`)
+  }
+
+  return (
+    <>
+      <div>
+        <input onChange={(e) => setChainTicker(e.target.value)} defaultValue={account?.author?.avatar?.chainTicker} placeholder="chain ticker e.g. eth, matic..." />
+        <input onChange={(e) => setTokenAddress(e.target.value)} defaultValue={account?.author?.avatar?.address} placeholder="token address e.g. 0x..." />
+        <input onChange={(e) => setTokenId(e.target.value)} defaultValue={account?.author?.avatar?.id} placeholder="token id e.g. 1001" />
+        <button onClick={copyMessageToSign}>copy message to sign</button>
+        <div>
+          go to{' '}
+          <a href="https://etherscan.io/verifiedSignatures" target="_blank" rel="noreferrer">
+            https://etherscan.io/verifiedSignatures
+          </a>{' '}
+          and sign, then paste signature
+        </div>
+      </div>
+      <div>
+        <input onChange={(e) => setSignature(e.target.value)} defaultValue={account?.author?.avatar?.signature?.signature} placeholder="signature e.g. 0x..." />
+        <button onClick={save}>save</button>
+      </div>
+      <NftAvatarPreview avatar={avatar} />
+    </>
+  )
+}
+
+const NftAvatar = () => {
+  const account = useAccount()
+  if (!account) {
+    return (
+      <div>
+        <div>avatar:</div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div>avatar:</div>
+      <NftAvatarForm account={account} />
+    </div>
+  )
+}
+
+export default NftAvatar
