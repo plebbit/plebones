a93add9f532e835dabf5fb502d9b778d6359a3ba
Esteban Abaroa <estebanabaroa@protonmail.com>
2025-08-12 03:53:06 +0000

    chore: more docs

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-const {contextBridge, ipcRenderer} = require('electron')
+const {contextBridge, ipcRenderer} = require('electron') // electron preload can't use import
 
 // dev uses http://localhost, prod uses file://...index.html
 const isDev = window.location.protocol === 'http:'
@@ -8,7 +8,8 @@ const defaultPlebbitOptions = {
 }
 
 contextBridge.exposeInMainWorld('defaultPlebbitOptions', defaultPlebbitOptions)
-contextBridge.exposeInMainWorld('defaultMediaIpfsGatewayUrl', 'http://localhost:6473')
+// plebbit doesn't run the ipfs dht, nfts are only seeded using ipfs dht, so can't use localhost
+// contextBridge.exposeInMainWorld('defaultMediaIpfsGatewayUrl', 'http://localhost:6473')
 
 // receive plebbit rpc auth key from main
 ipcRenderer.on('plebbit-rpc-auth-key', (event, plebbitRpcAuthKey) => contextBridge.exposeInMainWorld('plebbitRpcAuthKey', plebbitRpcAuthKey))
